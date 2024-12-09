ca69e6a013abfabbd43c731f4261bb8854071ffb
jeffgrahamcodes <jeffgrahamcodes@gmail.com>
2024-12-08 21:03:01 -0500

    Bonus material submission

--- week03/donations_pkg/homepage.py
+++ week03/donations_pkg/homepage.py
@@ -1,7 +1,7 @@
 # Task 2: Show homepage and initialize app data
 def show_homepage():
     print("")
-    print("          === DonateMe Homepage ===       ")
+    print("         === DonateMe Homepage ===          ")
     print("--------------------------------------------")
     print("| 1.    Login       | 2.    Register       |")
     print("--------------------------------------------")
@@ -11,17 +11,24 @@ def show_homepage():
     print("|                5. Exit                   |")
     print("--------------------------------------------")
 
+# Task 6: Donate functionality
 def donate(username):
     donation_amt = float(input('Enter amount to donate: '))
     donation_string = f'{username} donated ${donation_amt:.2f}'
-    print('Thank you!')
+    print('Thank you for your donation!')
     return donation_string
 
+# Task 7: Show Donations functionality
 def show_donations(donations):
-    print('"\n--- All Donations ---"')
+    print('\n         --- All Donations ---            ')
 
     if len(donations) == 0:
         print("Currently, there are no donations.")
     else:
+        total = 0.0
         for donation in donations:
             print(donation)
+            current_donation_amt = float(donation[donation.index('$') + 1:])
+            total = total + current_donation_amt
+        
+        print(f'Total = ${total:.2f}')
--- week03/donations_pkg/user.py
+++ week03/donations_pkg/user.py
@@ -1,18 +1,24 @@
+
+# Task 4: Login functionality
 def login(database, username, password):
-    if username in database.keys() and database[username] == password:
-        print(f'Welcome {username}')
+    if username.lower() in database.keys() and database[username.lower()] == password:
+        print(f'\nWelcome back {username}!')
         return username
-    elif username in database.keys() and database[username] != password:
-        print('Invalid password')
+    elif username.lower() in database.keys() and database[username] != password:
+        print(f'\nIncorrect password for {username}')
         return ''
     else:
         print('User not found. Please register.')
 
+# Task 5: Register functionality
 def register(database, username):
-    if username in database.keys():
+    if username.lower() in database.keys():
         print('Username already registered')
         return ''
     else: 
+        if len(username) > 10:
+            print('username must not exceed 10 characters')
+            return ''
         print(f'{username} has been registered')
         return username
 
