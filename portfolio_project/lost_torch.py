94b4826de4c79825283887108f29474ba28f5f8a
J. Graham <polymathy78@gmail..com>
2024-12-20 17:35:49 -0500

    refactor code

--- portfolio_project/lost_torch.py
+++ portfolio_project/lost_torch.py
@@ -1,20 +1,15 @@
 class Player:
     def __init__(self, start_room):
-        # The room where the player begins the game
         self.current_room = start_room
-        # A list to hold items the player picks up
         self.inventory = []
 
     def move(self, new_room):
-        # Update the player’s current room
         self.current_room = new_room
 
     def pick_up_item(self, item):
-        # Add an item to the player’s inventory
         self.inventory.append(item)
 
     def has_item(self, item):
-        # Check if the player has a certain item
         return item in self.inventory
 
 
@@ -45,7 +40,66 @@ rooms = {
 
 player = Player('Entrance Hall')
 
+
+def move_player(direction):
+    current = player.current_room
+    if direction not in rooms[current]['exits']:
+        print('Cannot go that way\n')
+        return
+
+    new_room = rooms[current]['exits'][direction]
+
+    # Check if the new room is the Exit and if the player has the treasure
+    if new_room == "Exit" and not player.has_item('treasure'):
+        print("The exit is locked! You need the treasure to leave.\n")
+        return
+
+    player.move(new_room)
+
+    # If the player reaches the Exit, show its description and end the game loop
+    if player.current_room == 'Exit':
+        print(rooms[player.current_room]['description'])
+        # Potentially break out of the loop or return a flag to end game
+        return
+
+    print(f'You enter the {player.current_room}\n')
+
+
+def show_room_description():
+    print(rooms[player.current_room]['description'] + '\n')
+
+
+def show_inventory():
+    if player.inventory:
+        print("You have the following items:")
+        for i in player.inventory:
+            print(i)
+        print()
+    else:
+        print("Your inventory is empty.\n")
+
+
+def show_room_items():
+    if rooms[player.current_room]['items']:
+        print('You see the following item(s) in the room:')
+        for item in rooms[player.current_room]['items']:
+            print(item)
+        print()
+    else:
+        print('The room is empty.\n')
+
+
+def pick_up_item():
+    if rooms[player.current_room]['items']:
+        item = rooms[player.current_room]['items'].pop()
+        player.pick_up_item(item)
+        print(f'You pick up the {item} and add it to your inventory.\n')
+    else:
+        print('There is nothing to pick up here.\n')
+
+
 while True:
+    # Display options
     print("1) Get room description")
     print("2) Check Inventory")
     print("3) Check Room")
@@ -54,50 +108,25 @@ while True:
     print("S) Move South")
     print("E) Move East")
     print("W) Move West")
-    option = input("What would do want to do: ").upper()
+
+    option = input("What would you like to do? ").upper().strip()
+    print()
 
     if option == "1":
-        print(rooms[player.current_room]['description'] + '\n')
-    if option == "2":
-        if player.inventory:
-            print(len(player.inventory))
-        else:
-            print("inventory empty")
-    if option == "3":
-        if rooms[player.current_room]['items']:
-            print('You see the following item(s) in the room:')
-            for item in rooms[player.current_room]['items']:
-                print(item)
-        else:
-            print('room empty')
-    if option == "4":
-        if rooms[player.current_room]['items']:
-            item = rooms[player.current_room]['items'].pop()
-            player.pick_up_item(item)
-            print(f'You pick up the {item} amd add it to your inventory')
-        else:
-            print('nothing to pick up in this room')
-    if option == 'N':
-        if 'north' in rooms[player.current_room]['exits']:
-            player.move(rooms[player.current_room]['exits']['north'])
-            print(f'You enter the {player.current_room}\n')
-        else:
-            print('Cannot go that way')
-    if option == 'S':
-        if 'south' in rooms[player.current_room]['exits']:
-            player.move(rooms[player.current_room]['exits']['south'])
-            print(f'You enter the {player.current_room}\n')
-        else:
-            print('Cannot go that way')
-    if option == 'E':
-        if 'east' in rooms[player.current_room]['exits']:
-            player.move(rooms[player.current_room]['exits']['east'])
-            print(f'You enter the {player.current_room}\n')
-        else:
-            print('Cannot go that way')
-    if option == 'W':
-        if 'west' in rooms[player.current_room]['exits']:
-            player.move(rooms[player.current_room]['exits']['west'])
-            print(f'You enter the {player.current_room}\n')
-        else:
-            print('Cannot go that way')
+        show_room_description()
+    elif option == "2":
+        show_inventory()
+    elif option == "3":
+        show_room_items()
+    elif option == "4":
+        pick_up_item()
+    elif option == 'N':
+        move_player('north')
+    elif option == 'S':
+        move_player('south')
+    elif option == 'E':
+        move_player('east')
+    elif option == 'W':
+        move_player('west')
+    else:
+        print("Invalid choice. Please try again.\n")
